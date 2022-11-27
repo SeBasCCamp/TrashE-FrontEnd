d9ca1c09d95e85acd3dbad46f66d9cfa5e2968f6
fredy chávez campos <fschavezc@gmail.com>
2022-11-27 17:25:37 -0500

    test

--- src/test/Login.test.js
+++ src/test/Login.test.js
@@ -1,11 +1,12 @@
 import {render, screen} from '@testing-library/react'
 import Login from '../componentes/Login'
+import '@testing-library/jest-dom'
 describe('Login component', () =>{
 
     //Si se encuentra en el doc
-    test('El input del login se encuentra en el documento', async () =>{
+    test('El input del login se encuentra en el documento', () =>{
         render(<Login />);
-        const inputUsername = screen.getByRole('heading')
+        const inputUsername = screen.getByLabelText('Ingrese su correo')
         expect(inputUsername).toBeInTheDocument();
     })
 
